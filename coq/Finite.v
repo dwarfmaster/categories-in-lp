506028efbcf29090a4db5cbc0063a50994ee07f1
DwarfMaster <luc@dwarfmaster.net>
2021-12-02 15:31:01 +0100

    Refactored Finite so it's not a GADT anymore

--- coq/Finite.v
+++ coq/Finite.v
@@ -2,32 +2,56 @@
 From HoTT Require Import Basics.
 From HoTT Require Import Spaces.Nat.
 
-Inductive Fin : nat -> Type :=
-| F1 (n : nat) : Fin (S n)
-| FS (n : nat) : Fin n -> Fin (S n).
+Inductive Fin (n : nat) : Type :=
+| F1 (m : nat) : n = S m -> Fin n
+| FS (m : nat) : n = S m -> Fin m -> Fin n.
+
+Section NatLemmas.
+  Definition pred (n : nat) : nat :=
+    match n with
+    | O => O
+    | S n => n
+    end.
+
+End NatLemmas.
 
 Section FinTypes.
-  Variables (n m : nat).
 
   Fixpoint fin_to_nat_rec {n : nat} (k : nat) (x : Fin n) : nat :=
     match x with
-    | F1 _ => k
-    | FS _ x => S (fin_to_nat_rec k x)
+    | F1 _ _ => k
+    | FS _ _ x => S (fin_to_nat_rec k x)
     end.
   (* Interprets Fin n as the type [0..n-1] *)
   Definition fin_to_nat {n : nat} (x : Fin n) : nat := fin_to_nat_rec 0 x.
   (* Interprets Fin n as the type [1..n] *)
   Definition fin_to_nat1 {n : nat} (x : Fin n) : nat := fin_to_nat_rec 1 x.
 
-  Fixpoint add_fin {m : nat} (n : nat) (y : Fin m) : Fin (n + m)%nat :=
+  Definition add_fin {m : nat} (n : nat) (y : Fin m) : Fin (n + m)%nat.
+  Proof.
+    induction n; simpl.
+    - exact y.
+    - eapply FS.
+      + reflexivity.
+      + exact IHn.
+  Defined.
+  Definition inj_fin {m : nat} (n : nat) (y : Fin m) : Fin (m + n)%nat.
+  Proof.
+    induction y; simpl in *.
+    - apply (F1 _ (m + n)%nat). rewrite p. reflexivity.
+    - apply (FS _ (m + n)%nat); [ idtac | exact IHy ].
+      rewrite p. reflexivity.
+  Defined.
+
+  Definition inj_pred {k : nat} (y : Fin (pred k)) : Fin k.
+  Proof.
+    destruct k; [ exact y | idtac ].
+    simpl in y. apply (FS _ k); [ reflexivity | exact y ].
+  Defined.
+  Fixpoint last_fin (n : nat) : Fin (S n) :=
     match n with
-    | O => y
-    | S n => FS _ (add_fin n y)
-    end.
-  Fixpoint inj_fin {m : nat} (n : nat) (y : Fin m) : Fin (m + n)%nat :=
-    match y with
-    | F1 m => F1 (m + n)%nat
-    | FS m y => FS (m + n)%nat (inj_fin n y)
+    | O => F1 1 0 1
+    | S n => FS (S (S n)) (S n) 1 (last_fin n)
     end.
 
   Definition pack_add (n m : nat) (e : Fin n + Fin m) : Fin (n + m)%nat :=
@@ -36,14 +60,16 @@ Section FinTypes.
     | inr y => add_fin n y
     end.
 
-  Lemma unpack_add (k l : nat) (f : Fin (k + l)%nat) : Fin k + Fin l.
+  Definition unpack_add (k l : nat) (f : Fin (k + l)%nat) : Fin k + Fin l.
   Proof.
-    clear n m. destruct k.
+    induction k.
     - right. exact f.
-    - induction f.
-      + left. constructor.
-      + exact IHf.
-  Qed.
+    - simpl in f. destruct f.
+      + left. apply (F1 _ k). reflexivity.
+      + apply Nat.path_nat_S in p. rewrite <- p in f. destruct (IHk f).
+        * left. apply (FS _ k); [ reflexivity | exact f0 ].
+        * right. exact f0.
+  Defined.
 
   Lemma PackEquiv : forall n m, IsEquiv (pack_add n m).
   Proof.
