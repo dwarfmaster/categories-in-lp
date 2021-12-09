4263613297e2360b715a55204fbb904158dce299
DwarfMaster <luc@dwarfmaster.net>
2021-12-09 16:31:31 +0100

    Define isomorphism between traditional diagram and graph

--- coq/FiniteCategory.v
+++ coq/FiniteCategory.v
@@ -1,22 +1,36 @@
 
 From HoTT Require Import Basics.
 From HoTT Require Import Categories.
-Require Import Finite.
+From HoTT Require Import Spaces.Finite.
+Require Import Graph.
 
-Record IsFinitePreCategory (C : PreCategory) :=
-  mkFPC
-    { fpc_size : nat
-    ; fpc_hom_size (a b : object C) : nat
-    ; fpc_equiv : object C <~> Fin fpc_size
-    ; fpc_hom_equiv (a b : object C) : morphism C a b <~> Fin (fpc_hom_size a b)
+Definition Arrows (C : PreCategory) : Type :=
+  exists(a b : object C), morphism C a b.
+
+Include FunctorCoreNotations.
+
+Definition eq_to_iso {C : PreCategory} {x y : object C} (p : x = y) : morphism C x y.
+Proof.
+  rewrite p. exact 1%morphism.
+Defined.
+
+Local Open Scope morphism.
+
+Record GraphDiagram {size arrows : nat} {C I : PreCategory}
+       {D : Functor I C} {G : Graph C size arrows} :=
+  mkGraphDiagram
+    { ftgr_objs : Fin size -> object I
+    ; ftgr_objs_equiv : IsEquiv ftgr_objs
+    ; ftgr_vertices : Fin arrows -> Arrows I
+    ; ftgr_vertices_equiv : IsEquiv ftgr_vertices
+    ; ftgr_src_comm : forall(a : Fin arrows), ftgr_objs (gr_src G a) = proj1 (ftgr_vertices a)
+    ; ftgr_dst_comm : forall(a : Fin arrows), ftgr_objs (gr_dst G a) = proj1 (proj2 (ftgr_vertices a))
+    ; ftgr_objs_comm : forall(v : Fin size), (D _0 (ftgr_objs v))%object = gr_vertex G v
+    ; ftgr_edge_comm : forall(a : Fin arrows), D _1 (proj2 (proj2 (ftgr_vertices a)))
+                                          = D _1 (eq_to_iso (ftgr_dst_comm a))
+                                            o eq_to_iso (ftgr_objs_comm (gr_dst G a))^
+                                            o gr_edge G a
+                                            o eq_to_iso (ftgr_objs_comm (gr_src G a))
+                                            o D _1 (eq_to_iso (ftgr_src_comm a)^)
     }.
 
-Record Arrows (C : PreCategory) :=
-  mkArrows
-    { arr_src : object C
-    ; arr_dst : object C
-    ; arr_mph : morphism C arr_src arr_dst
-    }.
-
-(* TODO *)
-(* Lemma FiniteArrows (C : PreCategory) : IsFinitePreCategory C -> exists n, Arrows C <~> Fin n. *)
