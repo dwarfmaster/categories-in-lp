4d0815c6e6fcf6754f329ebf37365a6d86b17704
DwarfMaster <luc@dwarfmaster.net>
2022-01-11 14:51:26 +0100

    Improve build system

---
+++ coq/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+export FILES=(\
+  Misc.v \
+  Limits/Graph.v \
+  Limits/Functor.v \
+  Limits/Equalizer.v \
+  Limits/Product.v \
+  Limits/Pullback.v \
+  Limits/Terminal.v \
+  Limits/Extension.v \
+  Slice/Misc.v \
+  Slice/BaseChangeFunctor.v \
+  Cartesian.v \
+  Exponential.v \
+  CartesianClosed.v \
+)
+
+echo $FILES
+for f in ${FILES[@]}; do
+  coqc -R . Categories -noinit -indices-matter $f
+done
+
