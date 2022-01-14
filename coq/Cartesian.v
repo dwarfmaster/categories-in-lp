964982253cd1a07e6f8f1aa13f54d4b425b97b19
DwarfMaster <luc@dwarfmaster.net>
2022-01-14 14:02:59 +0100

    Show that LCC have local pullbacks

--- coq/Cartesian.v
+++ coq/Cartesian.v
@@ -211,3 +211,106 @@ Proof.
     apply LocallyCartesianHasPullbacks. exact LCC.
   - exact (H 0%nat 0%nat (TerminalGr C)).
 Qed.
+
+Module LCCHasLocalPullbacks.
+  Section LCCHasLocalPullbacks.
+    Context `{Funext}.
+    Context {C : PreCategory}.
+    Context (LCC : IsLocallyCartesian C).
+    Context {Y : object C}.
+    Context {x y z : object (C/Y)}.
+    Context (f : morphism (C/Y) x z) (g : morphism (C/Y) y z).
+
+    Definition X : object C := Slice.s z.
+    Definition f_X : object (C/X) := Build_SliceObject (Slice.m f).
+    Definition g_X : object (C/X) := Build_SliceObject (Slice.m g).
+    Definition fXg : Product f_X g_X := snd (LCC X) f_X g_X.
+
+    Definition PbCone : Cone (ProductGr f_X g_X) -> Cone (PullbackGr f g).
+    Proof.
+      intro cn. srapply mkCone.
+      - srapply Build_SliceObject; [ exact (Slice.s (cn_top cn)) | ].
+        exact (Slice.f z o Slice.m f o Slice.m (cn_side cn fin1)).
+      - intro n; destruct3 n.
+        + srapply Build_SliceMorphism; [ exact (Slice.m f o Slice.m (cn_side cn fin1)) | ].
+          rewrite <- associativity. reflexivity.
+        + srapply Build_SliceMorphism; [ exact (Slice.m (cn_side cn fin2)) | ].
+          simpl. rewrite associativity. change (Slice.m f) with (Slice.f f_X).
+          rewrite (slice_m_comm (cn_side cn fin1)). rewrite <- (slice_m_comm (cn_side cn fin2)).
+          rewrite <- associativity. change (Slice.f g_X) with (Slice.m g).
+          rewrite (slice_m_comm g). reflexivity.
+        + srapply Build_SliceMorphism; [ exact (Slice.m (cn_side cn fin1)) | ]. simpl.
+          rewrite <- (slice_m_comm f). reflexivity.
+      - intro n; destruct2 n; apply slice_m_injective; simpl; try reflexivity.
+        change (gr_edge (PullbackGr f g) (inr tt)) with g.
+        change (Slice.m f) with (Slice.f f_X). rewrite (slice_m_comm (cn_side cn fin1)).
+        rewrite <- (slice_m_comm (cn_side cn fin2)). reflexivity.
+    Defined.
+    Definition PdCone : Cone (PullbackGr f g) -> Cone (ProductGr f_X g_X).
+    Proof.
+      intro cn. srapply mkCone.
+      - srapply Build_SliceObject; [ exact (Slice.s (cn_top cn)) | ].
+        exact (Slice.f f_X o Slice.m (cn_side cn f1)).
+      - intro n; destruct2 n.
+        + srapply Build_SliceMorphism; [ exact (Slice.m (cn_side cn f2)) | ].
+          simpl. rewrite <- slice_m_comp. rewrite (cn_comm cn fin1).
+          rewrite <- (cn_comm cn fin2). reflexivity.
+        + srapply Build_SliceMorphism; [ exact (Slice.m (cn_side cn f1)) | ].
+          reflexivity.
+      - intro n. empty_ind.
+    Defined.
+
+    Definition ConeMorphismPbToPd c1 c2 :
+      ConeMorphism (PdCone c1) c2 ->
+      ConeMorphism c1 (PbCone c2).
+    Proof.
+      intro mph. srapply mkCnMph.
+      - srapply Build_SliceMorphism; [ exact (Slice.m (cnmph_mph mph)) | ].
+        simpl. rewrite slice_m_comm. rewrite associativity.
+        rewrite <- (slice_m_comp (cnmph_mph mph) (cn_side c2 fin1)).
+        rewrite cnmph_comm. simpl. rewrite slice_m_comm. reflexivity.
+      - intro n; apply slice_m_injective; rewrite slice_m_comp; destruct3 n.
+        + rewrite associativity. rewrite <- (slice_m_comp (cnmph_mph mph) (cn_side c2 fin1)).
+          rewrite cnmph_comm. simpl. rewrite <- slice_m_comp. rewrite (cn_comm c1 fin2).
+          reflexivity.
+        + rewrite <- (slice_m_comp (cnmph_mph mph) (cn_side c2 fin2)).
+          rewrite cnmph_comm. reflexivity.
+        + rewrite <- (slice_m_comp (cnmph_mph mph) (cn_side c2 fin1)).
+          rewrite cnmph_comm. reflexivity.
+    Defined.
+    Definition ConeMorphismPdToPb c1 c2 :
+      ConeMorphism c1 (PbCone c2) ->
+      ConeMorphism (PdCone c1) c2.
+    Proof.
+      intro mph. srapply mkCnMph.
+      - srapply Build_SliceMorphism; [ exact (Slice.m (cnmph_mph mph)) | ].
+        simpl. rewrite <- (slice_m_comm (cn_side c2 fin2)). simpl.
+        rewrite <- (slice_m_comp (cn_side c1 f1) f).
+        rewrite (cn_comm c1 fin2). rewrite <- (cn_comm c1 fin1). rewrite slice_m_comp.
+        change (gr_edge (PullbackGr f g) fin1) with g.
+        rewrite <- (cnmph_comm mph f2). rewrite slice_m_comp. apply associativity.
+      - intro n; apply slice_m_injective; rewrite slice_m_comp; destruct2 n.
+        + rewrite <- (cnmph_comm mph f2). rewrite slice_m_comp. reflexivity.
+        + rewrite <- (cnmph_comm mph f1). rewrite slice_m_comp. reflexivity.
+    Defined.
+
+    Definition PbLimit : Limit (PullbackGr f g).
+    Proof.
+      srapply mkLim; [ exact (PbCone (lim_cone fXg)) | | ].
+      - intro cn. apply ConeMorphismPbToPd. apply lim_ex.
+      - intros c m1 m2.
+        pose (mph1 := ConeMorphismPdToPb m1). pose (mph2 := ConeMorphismPdToPb m2).
+        pose (Huniq := lim_uniq fXg _ mph1 mph2). apply slice_m_injective.
+        change (Slice.m (cnmph_mph m1)) with (Slice.m (cnmph_mph mph1)).
+        change (Slice.m (cnmph_mph m2)) with (Slice.m (cnmph_mph mph2)).
+        exact (ap Slice.m Huniq).
+    Defined.
+  End LCCHasLocalPullbacks.
+End LCCHasLocalPullbacks.
+
+Theorem LCCHasLocalPullbacks {C : PreCategory} (LCC : IsLocallyCartesian C) :
+  forall(Y : object C), AllPullbacks (C/Y).
+Proof.
+  intros Y a b c f g. apply LCCHasLocalPullbacks.PbLimit. exact LCC.
+Defined.
+Arguments LCCHasLocalPullbacks [C] (LCC Y).
