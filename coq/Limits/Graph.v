0682812b753843876c166f53d1c7093da3d2f6a8
DwarfMaster <luc@dwarfmaster.net>
2022-01-18 12:53:29 +0100

    Define path function for graphs

--- coq/Limits/Graph.v
+++ coq/Limits/Graph.v
@@ -58,6 +58,49 @@ Section paths.
   Context {Size Arrows : Type}.
   Context (G : Graph C Size Arrows).
 
+  Definition Graph_sig_T :=
+    { vertex : Size -> object C
+    | Arrows ->
+      { src : Size
+      & { dst : Size
+        & morphism C (vertex src) (vertex dst) }}}.
+  Lemma graph_sig : Graph_sig_T <~> Graph C Size Arrows.
+  Proof. issig. Qed.
+
+  Lemma path_Graph (G' : Graph C Size Arrows) :
+    forall(p_vertex : gr_vertex G = gr_vertex G'),
+    forall(p_src : forall(a : Arrows), (gr_edges G a).1 = (gr_edges G' a).1),
+    forall(p_dst : forall(a : Arrows), (gr_edges G a).2.1 = (gr_edges G' a).2.1),
+    (forall(a : Arrows),
+        transport
+          (fun V => morphism C (V (gr_edges G' a).1) (V (gr_edges G' a).2.1))
+          p_vertex
+          (transport (fun D => morphism C (gr_vertex G (gr_edges G' a).1) (gr_vertex G D))
+            ((ap pr1 (transport_sigma (p_src a) (gr_edges G a).2) @
+              transport_const (p_src a) ((gr_edges G a).2).1) @ p_dst a)
+            (transport (fun S => {dst : Size & morphism C (gr_vertex G S) (gr_vertex G dst)})
+               (p_src a) (gr_edges G a).2).2) = (gr_edges G' a).2.2) ->
+    G = G'.
+  Proof.
+    destruct G, G'; simpl. intros p_vertex p_src p_dst p_arrows. path_induction.
+    assert (gr_edges0 = gr_edges1) as p_edges.
+    { apply path_forall; intro a. apply (path_sigma _ _ _ (p_src a)). simpl in p_arrows.
+      srapply path_sigma; [ exact (ap pr1 (transport_sigma _ _) @ (transport_const _ _ @ p_dst a)) | ].
+      rewrite concat_p_pp. exact (p_arrows a). }
+    rewrite p_edges. reflexivity.
+  Qed.
+
+  (* Easy case *)
+  Lemma path_Graph_vertex (G0 G1 : Graph C Size Empty) :
+    forall(p_vertex : gr_vertex G0 = gr_vertex G1),
+      G0 = G1.
+  Proof.
+    destruct G0, G1; simpl; intro p_vertex. path_induction.
+    assert (gr_edges0 = gr_edges1) as p_edges.
+    { apply path_forall. empty_ind'. }
+    destruct p_edges. reflexivity.
+  Qed.
+
   Lemma path_Cone' (c1 c2 : Cone G) :
     forall(ptop : cn_top c1 = cn_top c2),
       (transport (fun X => forall(n : Size), morphism C X (gr_vertex G n)) ptop (cn_side c1) = cn_side c2) ->
